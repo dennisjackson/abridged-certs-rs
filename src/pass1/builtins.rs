1884e12654ff5360079192eae7b0e2450210097f
Dennis Jackson <88591716+dennisjackson@users.noreply.github.com>
2024-03-08 19:21:57 +0000

    Add compile-time generation of builtins from JSON

--- src/pass1/builtins.rs
+++ src/pass1/builtins.rs
@@ -1,6 +1,8 @@
 use super::CertIdentifier;
 use bytes::Bytes;
 
+include!(concat!(env!("OUT_DIR"), "/builtin_tables.rs"));
+
 pub fn cert_to_identifier(cert: &Bytes) -> Option<CertIdentifier> {
     todo!("Implement")
 }
